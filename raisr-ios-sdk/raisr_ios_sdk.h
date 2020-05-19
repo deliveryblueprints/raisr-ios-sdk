7f5ed4eb9d2f6430e221290bdf6898be43d98eb9
Nicky Thorne <nicky.thorne@deliveryblueprints.com>
2020-05-19 16:30:56 +0100

    Initial Commit

---
+++ raisr-ios-sdk/raisr_ios_sdk.h
@@ -0,0 +1,19 @@
+//
+//  raisr_ios_sdk.h
+//  raisr-ios-sdk
+//
+//  Created by Nicky Thorne on 19/05/2020.
+//  Copyright © 2020 Nicky Thorne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for raisr_ios_sdk.
+FOUNDATION_EXPORT double raisr_ios_sdkVersionNumber;
+
+//! Project version string for raisr_ios_sdk.
+FOUNDATION_EXPORT const unsigned char raisr_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <raisr_ios_sdk/PublicHeader.h>
+
+
